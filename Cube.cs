53b8e5af63c60059f658777143aece01ae09e1e7
Marty Mazeau <marty.mazeau@outlook.fr>
2021-01-15 12:14:21 +0100

    Add line in Cube.cs

--- Cube.cs
+++ Cube.cs
@@ -5,5 +5,7 @@ public class Cube : MonoBehaviour
 	private void Start()
 	{
 		Debug.log("Hello, I'm a Cube");
+		
+		tr.position = new Vector3(Random.Range(1;10,Random.Range(1;10,Random.Range(1;10));
 	}
 }

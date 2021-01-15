dd766ddba05627d253e9f064f2be50ee23e4825f
Emmanuel De Carvalho <emmanuel.decarvlho@gmail.com>
2021-01-15 12:25:09 +0100

    J ai remplacer une mot

--- Cube.cs
+++ Cube.cs
@@ -4,7 +4,7 @@ public class Cube : MonoBehaviour
 {
 	private void Start()
 	{
-		Debug.log("Hello, I'm a Cube");
+		Debug.log("Hi, I'm a Cube");
 		
 		tr.position = new Vector3(Random.Range(1;10),Random.Range(1;10),Random.Range(1;10));
 	}
